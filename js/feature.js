a5c6cae0ceaf7e71ed7a6e3c723e227d0c3e4266
ebnybuniad <ebnybuniad@hmail.com>
2025-02-28 23:37:16 +0600

    added current task

--- js/feature.js
+++ js/feature.js
@@ -4,6 +4,11 @@ const btnCompleteds = document.querySelectorAll('.btn-complete');
 for (const btnCompleted of btnCompleteds) {
     btnCompleted.addEventListener("click", function () {
         alert('Board updated Successfully');
+        const numberOfTasks = document.getElementById('numberOfTasks');
+        const INT_numberOfTasks = parseInt(numberOfTasks.innerText);
+        const currentTasks = INT_numberOfTasks - 1;
+        numberOfTasks.innerText = currentTasks;
+        console.log(currentTasks);
     })
 
 }
