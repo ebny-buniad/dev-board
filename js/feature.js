1121bb7aa382a480a30ea81f926abc023517a6ea
ebnybuniad <ebnybuniad@hmail.com>
2025-03-01 00:08:34 +0600

    updated total task

--- js/feature.js
+++ js/feature.js
@@ -4,12 +4,20 @@ const btnCompleteds = document.querySelectorAll('.btn-complete');
 for (const btnCompleted of btnCompleteds) {
     btnCompleted.addEventListener("click", function () {
         alert('Board updated Successfully');
-        const numberOfTasks = document.getElementById('numberOfTasks');
-        const INT_numberOfTasks = parseInt(numberOfTasks.innerText);
-        const currentTasks = INT_numberOfTasks - 1;
+        const tasks = convertInt('numberOfTasks');
+        const currentTasks = tasks - 1;
+        if (currentTasks < 0) {
+            return;
+        }
         numberOfTasks.innerText = currentTasks;
-        console.log(currentTasks);
+        const totalTask = convertInt('totalTasks');
+        const updatedTotalTask = totalTask + 1;
+        totalTasks.innerText = updatedTotalTask;
     })
-
 }
 
+function convertInt(id) {
+    const getID = document.getElementById(id);
+    const convertIntGetID = parseInt(getID.innerText)
+    return convertIntGetID;
+}
