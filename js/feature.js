b8dcd4cbc0fea36596a59e446279054232d1fc81
ebnybuniad <ebnybuniad@hmail.com>
2025-03-01 00:29:55 +0600

    added disbale fiture after click btn

--- js/feature.js
+++ js/feature.js
@@ -1,21 +1,26 @@
-console.log('Hello ')
-
 const btnCompleteds = document.querySelectorAll('.btn-complete');
 for (const btnCompleted of btnCompleteds) {
-    btnCompleted.addEventListener("click", function () {
+    btnCompleted.addEventListener("click", function (event) {
         alert('Board updated Successfully');
+        // Reduce Current Tasks
         const tasks = convertInt('numberOfTasks');
         const currentTasks = tasks - 1;
         if (currentTasks < 0) {
             return;
         }
         numberOfTasks.innerText = currentTasks;
+        // Updated Total Tasks
         const totalTask = convertInt('totalTasks');
         const updatedTotalTask = totalTask + 1;
         totalTasks.innerText = updatedTotalTask;
+        this.disabled = true;
+        event.target.classList.add('bg-gray-500');
     })
 }
 
+
+
+// Convert String number to NUMBER
 function convertInt(id) {
     const getID = document.getElementById(id);
     const convertIntGetID = parseInt(getID.innerText)
