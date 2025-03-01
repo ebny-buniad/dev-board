0661ff4eea5b0c6f387a0ae280d9b22b4107a17e
ebnybuniad <ebnybuniad@hmail.com>
2025-03-01 22:21:35 +0600

    added blog question

--- js/feature.js
+++ js/feature.js
@@ -31,6 +31,13 @@ for (const btnCompleted of btnCompleteds) {
         div.classList.add('bg-white', 'text-xs', 'p-2', 'rounded-md', 'mb-1');
         div.innerHTML = `<p>You have Complete The Task <span>${taskTitle}</span></p>`;
         historyContainer.appendChild(div);
+        const time = new Date();
+        const currentTime = time.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+        // console.log(currentTime);
+        const p = document.createElement('p');
+        p.innerText = 'AT ' + currentTime;
+        p.classList.add('mt-2', 'font-semibold');
+        div.appendChild(p);
 
         // Clear History
         document.getElementById('dlt-history-btn').addEventListener('click', function () {
@@ -39,31 +46,15 @@ for (const btnCompleted of btnCompleteds) {
     })
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Call color generate function 
-document.getElementById('theme-btn').addEventListener('click', function (event) {
+document.getElementById('theme-btn').addEventListener('click', function () {
     const htmlBodyBgColor = document.getElementById('html');
     htmlBodyBgColor.style.backgroundColor = generateRandomColor();
 })
 
 // Date find 
 document.getElementById('date').innerText = new Date().toDateString();
+// document.getElementById('date').innerText = new Date().toTime().slice();
 
 // Convert String number to NUMBER
 function convertInt(id) {
